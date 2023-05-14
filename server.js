befe47a106edd43c20e9aa47fa3047de24c3a8ca
Steeven Salony <steeven@iMac.local>
2023-05-13 21:15:43 -0400

    Added questions array

--- server.js
+++ server.js
@@ -23,3 +23,12 @@ db.connect(err => {
     console.log(`Server running on port ${PORT}`);
   });
 });
+
+const questions = [
+  {
+    type: 'list',
+    name: 'menu',
+    message: 'What would you like to do?',
+    choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role', 'Delete Department', 'Delete Role', 'Delete Employee']
+  }
+]
